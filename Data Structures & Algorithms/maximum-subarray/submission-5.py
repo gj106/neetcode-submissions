5a67e3724b38bd12e6874fa784c0c1f3c928b28b
Gaurav Jaiswal <43282813+gj106@users.noreply.github.com>
2026-08-31 23:30:52 -0700

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        i, j = 0, 0
+        sumarray = -100001
+        maxsumarray = -100001
+
+        while j < len(nums):
+            if nums[j] > sumarray + nums[j]:
+                sumarray = nums[j]
+            else:
+                sumarray = sumarray + nums[j]
+            maxsumarray = max(sumarray, maxsumarray)
+            j += 1
+        return maxsumarray
+
+        
